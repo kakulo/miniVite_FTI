e56de967cea4a7d6ca1b0f2b498ff4e570fc9761
Konstantinos Parasyris <parasyris1@llnl.gov>
2020-07-06 14:43:42 -0700

    Results

--- fti_64_32_fail.sh
+++ fti_64_32_fail.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-#SBATCH --mail-user=lguo4@ucmerced.edu
-#SBATCH --mail-type=ALL
-#SBATCH --export=ALL
 #SBATCH -t 00:15:00
-#SBATCH -p RM
 #SBATCH -o res_64_fail.txt
 #SBATCH -N 32
 #SBATCH --ntasks-per-node=2
-export PATH="/home/azguolu/software/ompi-4.0.0/bin/:$PATH"
-
-#mpirun -np 512 ./test_HPCCG config.L1.512.16.fi.fti 128 128 32 5 -level 1 -procfi || mpirun -np 512 ./test_HPCCG config.L1.512.16.fi.fti 128 128 32 5 -level 1 
+module load openmpi/4.0.0
+export LD_LIBRARY_PATH=/usr/workspace/koparasy/LENNY/usr/lib64/:$LD_LIBRARY_PATH
+mkdir -p /p/lustre1/koparasy/LENNY/
 mpirun -np 64 ./miniVite config.L1.64.32.fi.fti -p 3 -l -n 128000 -LEVEL 1 -CP 3
